05ec6f6b35d2c702f3efd9ffcf93a6947ebbadd2
Zhunis Gaziz <12zhunus@gmail.com>
2024-09-09 10:24:07 +0500

    Change default font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type {Metadata} from 'next';
-import localFont from 'next/font/local';
+import {Roboto} from 'next/font/google';
 import './globals.css';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin', 'cyrillic-ext'],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -25,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body className={`${roboto.className}`}>{children}</body>
     </html>
   );
 }
